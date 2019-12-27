fa7e741cc62968b305b5be75fdd553da1206bd84
429272-23 <429272-23>
2019-12-27 14:32:31 +0300

    2.0

--- index.php
+++ index.php
@@ -1,30 +1,54 @@
 <?php
 
-class B{
-
+Class Artem{								//линейное
+	protected $x;
+	protected function _construct($result = 0 ){
+		$this->x = $result;
 }
-
-
-
-class A extends B{
-	public $p3;
-	public $p2;
-	public $p1;
-	public $p4;
-	function  __construct($p1,$p2,$p3,$p4){
-		$this -> p1= $p1;
-		$this -> p2= $p2;
-		$this -> p3= $p3;
-		$this -> p4= $p4;
-	}
+	protected function getX() {
+		return $this->result;
 }
-	
-$p3 = new B();
-$p2 = new B();
-$p1 = new B();
-$p4 = new B(); 
-$p5 = new A($p3,$p4,$p1,$p2);
-	
-	
-	
+	protected function setX($val) {
+		return $this->x = $val;
+}
+	protected function rut($a,$b){
+		$this->x = ($b* -1)/$a;
+		return $this->x;
+}
+}
+Class  Wake extends  Artem {			//квадратное
+	protected function _construct($obj1, $obj2=null, $obj3=null, $result = 0){
+		$this->obj1 = $obj1;
+		$this->obj2 = $obj2;
+		$this->obj3 = $obj3;
+}
+protected function getObj1(){
+	return $this->obj1;
+}
+protected function setObj1($val){
+	$this->obj1 = $val;
+}
+protected function Disc($a,$b,$c){
+	return pow($b,2) - 4*$a*$c;
+}  
+protected function Cvad ($a,$b,$c){
+	$disc = $this->Disc($a,$b,$c);
+	if($disc>0){
+		$this->x = Array();
+		$x1 = (-1*$b+sqrt($disc))/(2*$a);
+		$x2 = (-1*$b-sqrt($disc))/(2*$a);
+		array_push($this->x, $x1, $x2);
+}
+	if($disc == 0){
+		$this->x = (-1*$b)/(2*$a);
+}		
+	return $this->x;
+}
+protected $obj1;
+protected $obj2;
+protected $obj3;
+}
+ 
  ?>
+ 
+ 
