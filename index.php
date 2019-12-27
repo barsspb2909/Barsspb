5503f429d3036f374fd36860393e4435a44221f0
429272-23 <429272-23>
2019-12-27 14:47:46 +0300

    4.0

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php
-ini_set('display_errors', 1);
-error_reporting(-1);
+
+ini_set("display_errors", 1);
+error_reporting (-1); 
+
+use Artem\ArtemException;
+use Artem\MyLog;
+use Artem\Linear;
+use Artem\Square;
+
 
 include 'core/EquationInterface.php';
 include 'core/LogInterface.php';
@@ -10,3 +17,20 @@ include 'Artem/MyLog.php';
 include 'Artem/Linear.php';
 include 'Artem/Square.php';
 
+
+
+	try{
+		echo 'Enter a, b, c' . "\n";
+		$num = [];
+		for($i = 0; $i < 3; $i++){
+			$num[$i] = readline("Value = ");	
+		}
+		MyLog::log("Vvedeno uravneniye: " . $num[0] . "x^2 + " . $num[1] . "x + " . $num[2] . " = 0");
+		$square = new Square();
+		$roots = $square->solve($num[0], $num[1], $num[2]);
+		MyLog::log("Korni uravneniye: " . implode(",", $roots) . "\n");
+	} catch(ArtemException $e) {
+		MyLog::log($e->getMessage());
+	}
+
+MyLog::write();
